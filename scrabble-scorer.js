5929b8b59c34394cdba830d4b30bb4a13190ef1f
Shedd32 <justinshedd21@gmail.com>
2023-10-14 15:30:18 -0500

    added functions of diffetent scoring methods

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -42,54 +42,83 @@ function initialPrompt(word) {
 console.log(oldScrabbleScorer(initialPrompt()));
 
 
-let simpleScorer= {simpleScoreFunc};
+let simpleScorer;
 const simpleScorerPoints = {
-   1:[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, X, Y, Z]
+   1: [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'],
 }
-function simpleScoreFunc(word){
+function simpleScore(word){
    word = word.toUpperCase();
-	let letterPoints = "";
+	let letterPoints = word.length;
  
-	for (let i = 0; i < word.length; i++) {
- 
-	  for (const pointValue in simpleScorerPoints) {
- 
-		 if (simpleScorerPoints[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
-		 }
-	}
 	return letterPoints;
-}
-}
+};
+console.log(simpleScore(initialPrompt()));
 
+let vowelBonusScorer;
 
-let vowelBonusScorer= {vowelBonusFunc};
 const vowelBonusPoints ={
-   1: [B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z]
-   3: [A, E, I, O, U]
+   1: [ 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'],
+   3: [ 'A', 'E', 'I', 'O', 'U']
 }
-function vowelBonusFunc(word){
+function vowelBonusScore(word){
    word = word.toUpperCase();
-   let points = "";
+   let letterPoints = [];
+   let sum = 0
+
    for (let i = 0; i < word.length; i++) {
  
-      for (const pointValue in vowelBonusScorer) {
+      for (const pointValue in vowelBonusPoints) {
   
-        if (vowelBonusScorer[pointValue].includes(word[i])) {
-          letterPoints += `Points for '${word[i]}': ${pointValue}\n`
-        }
+         if (vowelBonusPoints[pointValue].includes(word[i])) {
+            letterPoints.push(pointValue);
+
+            let letterPointsNum = parseInt(letterPoints[i]);
+            
+            sum += letterPointsNum;
+            
+         }
   
       }
     }
-    return letterPoints;
- }
+    return sum;
+ };
 
+ console.log(vowelBonusScore(initialPrompt()));
 
 let scrabbleScorer;
+scrabbleScorer = oldScrabbleScorer()
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [ simpleScoreObj, BonusVowelsObj, scrabbleObj];
+let simpleScoreObj = {
 
-function scorerPrompt() {}
+   "Name" : "Simple Score",
+   "Description" : "Each letter worth 1 point", 
+   "Score Function" : simpleScore(),
+
+}
+
+let BonusVowelsObj = {
+   
+   "Name" : "Bonus Vowels",
+   "Description" :"Vowels are 3 pts, consonants are 1 pt.",
+   "Score Function" : vowelBonusScore(),
+
+}
+
+let scrabbleObj = {
+
+   "Name" : "Scrabble",
+   "Description" : "The traditional scoring algorithm.",
+   "Score Function" : scrabbleScorer,
+
+}
+
+function scorerPrompt(scoringAlgorithms) {
+
+
+
+
+}
 
 function transform() {};
 
